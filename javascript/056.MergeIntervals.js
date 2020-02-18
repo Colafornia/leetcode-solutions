f16926e083b20431285223972c6040aa9bce524c
Colafornia <wangyilinchn@foxmail.com>
2020-02-18 22:18:16 +0800

    add 56

---
+++ javascript/056.MergeIntervals.js
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/merge-intervals/
+
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function (intervals) {
+    if (!intervals || !intervals.length) {
+        return intervals
+    }
+    let ans = []
+    intervals.sort((a, b) => a[0] !== b[0] ? a[0] - b[0] : a[1] - b[1])
+    let prev = intervals[0]
+    ans.push(prev)
+    for (const current of intervals) {
+        if (current[0] <= prev[1]) {
+            prev[1] = Math.max(prev[1], current[1])
+        } else {
+            ans.push(current)
+            prev = current
+        }
+    }
+    return ans
+};
