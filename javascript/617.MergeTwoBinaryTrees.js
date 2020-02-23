664962323f5a10d14575e8226988ee8c0bfe1ee4
Colafornia <wangyilinchn@foxmail.com>
2020-02-23 17:03:10 +0800

    Add 617. Merge Two Binary Trees [Easy]

---
+++ javascript/617.MergeTwoBinaryTrees.js
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/merge-two-binary-trees/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} t1
+ * @param {TreeNode} t2
+ * @return {TreeNode}
+ */
+var mergeTrees = function (t1, t2) {
+    if (!t1 && !t2) {
+        return null
+    }
+    if (!t2) {
+        return t1
+    }
+    if (!t1) {
+        return t2
+    }
+    let sum = t1 ? t1.val : 0
+    sum += t2 ? t2.val : 0
+    let newNode = new TreeNode(sum)
+    newNode.left = mergeTrees(t1.left, t2.left)
+    newNode.right = mergeTrees(t1.right, t2.right)
+    return newNode
+};
