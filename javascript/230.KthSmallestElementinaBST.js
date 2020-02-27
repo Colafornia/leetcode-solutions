7af691ac7fc86df50fe4c1f17389ccfe30cf096f
Colafornia <wangyilinchn@foxmail.com>
2020-02-27 23:19:48 +0800

    Update 230. Kth Smallest Element in a BST [Medium]

--- javascript/230.KthSmallestElementinaBST.js
+++ javascript/230.KthSmallestElementinaBST.js
@@ -13,6 +13,26 @@
  * @return {number}
  */
 var kthSmallest = function (root, k) {
+    let stack = []
+    let arr = []
+    let current = root
+    while (current || stack.length) {
+        while (current) {
+            stack.push(current)
+            current = current.left
+        }
+        current = stack.pop()
+        if (current) {
+            arr.push(current.val)
+        }
+        if (arr.length === k) {
+            return current.val
+        }
+        current = current.right
+    }
+};
+
+var kthSmallest2 = function (root, k) {
     var result = [];
     var traverseNode = function (node) {
         if (node) {
