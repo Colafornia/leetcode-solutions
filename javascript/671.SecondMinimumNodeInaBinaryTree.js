036b7917f1dfd12b68597e94d79fbc61a19f018c
Colafornia <wangyilinchn@foxmail.com>
2020-02-28 17:40:13 +0800

    Update 671. Second Minimum Node In a Binary Tree [Easy]

--- javascript/671.SecondMinimumNodeInaBinaryTree.js
+++ javascript/671.SecondMinimumNodeInaBinaryTree.js
@@ -11,18 +11,43 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var findSecondMinimumValue = function(root) {
-  var ans = [];
-  var traverseNode = function(node) {
-      if (node) {
-          if (!ans.includes(node.val)) {
-              ans.push(node.val);
-          }
-          traverseNode(node.left);
-          traverseNode(node.right);
-      }
-  }
-  traverseNode(root);
-  ans = ans.sort((a,b) => a-b);
-  return ans[1] || -1;
+
+var findSecondMinimumValue = function (root) {
+    if (!root) {
+        return -1
+    }
+    if (!root.left && !root.right) {
+        return -1
+    }
+    let left = root.left.val
+    let right = root.right.val
+    if (left === root.val) {
+        left = findSecondMinimumValue(root.left)
+    }
+    if (right === root.val) {
+        right = findSecondMinimumValue(root.right)
+    }
+    if (left !== -1 && right !== -1) {
+        return Math.min(left, right)
+    } else if (left === -1) {
+        return right
+    } else {
+        return left
+    }
+};
+
+var findSecondMinimumValue2 = function (root) {
+    var ans = [];
+    var traverseNode = function (node) {
+        if (node) {
+            if (!ans.includes(node.val)) {
+                ans.push(node.val);
+            }
+            traverseNode(node.left);
+            traverseNode(node.right);
+        }
+    }
+    traverseNode(root);
+    ans = ans.sort((a, b) => a - b);
+    return ans[1] || -1;
 };
