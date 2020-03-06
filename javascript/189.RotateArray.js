b5d626f5f6784d63a5e8165a097200b8d70f239e
Colafornia <wangyilinchn@foxmail.com>
2020-03-06 23:55:07 +0800

    Update 189. Rotate Array [Easy]

--- javascript/189.RotateArray.js
+++ javascript/189.RotateArray.js
@@ -15,6 +15,32 @@ var rotate = function (nums, k) {
     }
 };
 
-var rotate2 = function(nums, k) {
+var rotate2 = function (nums, k) {
     nums.unshift(...nums.splice(nums.length - k))
+}
+
+function rotate3(nums, k) {
+    if (!nums.length) return []
+
+    if (k > 0) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1)
+        reverse(nums, 0, k - 1)
+        reverse(nums, k, nums.length - 1)
+    }
+
+    return nums
+};
+
+function reverse(arr, start, end) {
+    if (!arr || !arr.length || start >= end) return
+
+    while (start < end) {
+        const temp = arr[start]
+        arr[start] = arr[end]
+        arr[end] = temp
+        start++, end--
+    }
+
+    return arr
 }
